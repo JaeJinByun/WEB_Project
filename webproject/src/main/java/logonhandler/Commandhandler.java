d5c1d2226ca2c70a87db168498d9d53c9edf3c90
JaeJinByun <103496262+JaeJinByun@users.noreply.github.com>
2022-05-18 11:03:45 +0900

    기본 베이스 파일들
    초안 업로드

---
+++ webproject/src/main/java/logonhandler/Commandhandler.java
@@ -0,0 +1,14 @@
+package logonhandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+
+/* handler 인터페이스  */
+/* 핸들러 호출은 무조건 커맨드핸들러를 거친다. */
+public interface Commandhandler {
+	
+	public ModelAndView Process(HttpServletRequest request, HttpServletResponse response) throws Exception;	
+	
+}
